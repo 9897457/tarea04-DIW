eb06adfdfaac1d13cde4f305f8b74961dea14e80
9897457 <9897457@alu.murciaeduca.es>
2022-01-23 22:51:18 +0100

    Version 001 - estructura

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="body">
+    <div id="zonaSuperior">
+        <Cabecera />
+        <Breadcrumb />
+    </div>
+    <div id="zonaMedia">
+      <div id="producto">
+        <Imagenes />
+        <div id="datosProducto">
+          <DatosMasContador />
+        </div>
+      </div>
+        <Descripcion />
+    </div>
+    <Pie />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cabecera from './components/Cabecera.vue'
+import Breadcrumb from './components/Breadcrumb.vue'
+import Imagenes from './components/Imagenes.vue'
+import DatosMasContador from './components/DatosMasContador.vue'
+import Descripcion from './components/Descripcion.vue'
+import Pie from './components/Pie.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cabecera,
+    Breadcrumb,
+    Imagenes,
+    DatosMasContador,
+    Descripcion,
+    Pie
   }
 }
 </script>
@@ -23,4 +48,32 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#body {
+  display: flex;
+  flex-direction: column;
+
+}
+
+#zonaSuperior {
+  display: flex;
+  flex-direction: column;
+}
+
+#zonaMedia {
+  display: flex;
+  flex-direction: column;
+}
+
+#producto {
+  display: flex;
+  flex-direction: row;
+}
+
+#datosProducto {
+  display: flex;
+  flex-direction: column;
+}
+
+
 </style>
---
+++ src/components/Pie.vue
@@ -0,0 +1,15 @@
+<template>
+ <footer id="pie">
+    <p>© Copyright 2022 - Todos los derechos reservados </p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

f80aa4fe9c941e31128e3c1aa11cd8938830c8f9
9897457 <9897457@alu.murciaeduca.es>
2022-01-25 20:53:51 +0100

    Version 002.2 - Cabecera

--- src/components/Cabecera.vue
+++ src/components/Cabecera.vue
@@ -1,8 +1,20 @@
 <template>
   <section>
-      <img src="../assets/logo.png" alt="Logo de la empresa">
-      <div id="menu">
-        <nav>
+      <div id="menus">
+        <div id="menu1">
+          <img src="../assets/logo.png" alt="Logo de la empresa">
+          <form @submit.prevent="handleEnviar">
+            <input type="text" v-model="buscar" placeholder="Buscar productos ...">
+            <button id="boton">Buscar</button>
+          </form>
+          <nav>
+            <ul>
+              <li><a href="#">REGISTRO</a></li>
+              <li><a href="#">INICIAR SESIÓN</a></li>
+            </ul>
+          </nav>
+        </div>
+        <nav id="menu2">
           <ul>
             <li><a href="#">INICIO</a></li>
             <li><a href="#">ORDENADORES</a></li>
@@ -10,16 +22,6 @@
             <li><a href="#">MÓVILES</a></li>
             <li><a href="#">TABLETS</a></li>
             <li><a href="#">MULTIMEDIA</a></li>
-          </ul>
-        </nav>
-        <form @submit.prevent="handleEnviar">
-          <input type="text" v-model="buscar" placeholder="Buscar productos ...">
-          <button id="boton">Buscar</button>
-        </form>
-        <nav>
-          <ul>
-            <li><a href="#">REGISTRO</a></li>
-            <li><a href="#">INICIAR SESIÓN</a></li>
           </ul>
         </nav>
       </div>
@@ -47,49 +49,57 @@ export default {
 <style scoped>
   section img {
     display: flex;
-    padding-bottom: 1rem;
+    padding-right: 1rem;
+    padding-left: 1.5rem;
   }
 
-  #menu {
-    background: #1abc9c;
+  #menus {
+    border: 2px solid #000a08;
     z-index:999;
     padding:0.7rem;
     display: flex;
-    justify-content: space-between;
-    align-items: center;
+    flex-direction: column;
   }
 
-  #menu nav ul {
+  #menus nav ul {
     display:flex;
     list-style: none;
     padding: 0;
     margin: 0rem 0.7rem;
   }
 
-  #menu nav ul li a {
+  #menus nav ul li a {
     display: flex;
     justify-content: space-between;
     text-decoration: none;
-    color:white;
+    color:#1abc9c;
     font-weight: 700;
     padding: 0.5rem 1rem;
   }
 
-  #menu nav ul li a:hover, #boton:hover{
+  #menus nav ul li a:hover, #boton:hover{
     background-color:black;
     text-decoration: none;
     color:white;
     font-weight: 700;
   }
 
-  #menu form input{
+  #menus form input{
+    display: flex;
+    flex-grow: 1;
     padding: 0.7rem 1rem;
-    border-style: solid;
     border: 2px solid #1abc9c;
     color: #181818;
     outline: none;
     line-height: 1rem;
     font-size: 1rem;
+    
+  }
+
+  form {
+    display: flex;
+    padding: 1rem;
+    flex-grow: 1;
   }
 
   #boton {
@@ -100,5 +110,11 @@ export default {
     border: none;
     background-color: #1abc9c;
 
+  }
+
+  #menu1 {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
   }
 </style>
